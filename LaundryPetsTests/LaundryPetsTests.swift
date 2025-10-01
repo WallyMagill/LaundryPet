41d4940e04707586dd1aee022af8418add20633b
Walter Magill <wallymagill15@gmail.com>
2025-10-01 16:22:38 -0600

    Initial Commit

---
+++ LaundryPetsTests/LaundryPetsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LaundryPetsTests.swift
+//  LaundryPetsTests
+//
+//  Created by Walter Magill on 10/1/25.
+//
+
+import Testing
+@testable import LaundryPets
+
+struct LaundryPetsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
